351eac7b2a3758c9c92935c8148a2b8f6f613edc
ouarzy <epecoul@gmail.com>
2017-03-21 11:37:03 +0100

    Initial project

---
+++ RecoNoscoMinimalExample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using RSDK.Reporting;
+
+namespace ToDelete
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object _reporting;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _reporting = ReportingFactory.Reporting;
+                MessageBox.Show("ok");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"fail {exception?.InnerException?.Message}");
+            }
+        }
+    }
+}
